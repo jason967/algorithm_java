0b0e023fceeade9d2307399c01ba049969d0ba51
jason967 <jason967@naver.com>
2020-10-30 00:13:53 +0900

    1030

--- src/com/swea/solution_2048.java
+++ src/com/swea/solution_2048.java
@@ -17,57 +17,44 @@ public class solution_2048 {
         Deque<Integer> F;
         Deque<Integer> B;
         int[][] CA = new int[N][N];
-        for(int i=0;i<N;i++)
-        {
-            F= new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            F = new ArrayDeque<>();
             B = new ArrayDeque<>();
-            for(int j=0;j<N;j++) F.addLast(A[i][j]);
-            while(F.size()>0)
+            for (int j = 0; j < N; j++)
             {
+                if(A[i][j]==0) continue;
+                F.addLast(A[i][j]);
+            }
+            while (F.size() > 0) {
                 int head = F.pollLast();
-                if(head==0) continue;
-                if(F.size()==1)
-                {
-                    int temp = F.pollLast();
-                    B.addFirst(temp);
+                if (F.size() == 0) {
+                    B.addFirst(head);
                     break;
                 }
                 int tail = F.peekLast();
-                System.out.println("head: "+ head+"tail: "+tail);
-                if(head==tail)
-                {
-                    B.addFirst(head+tail);
+                if (head == tail) {
+                    B.addFirst(head + tail);
                     F.pollLast();
-                }
-                else
-                {
-                    B.addFirst(head);
-                }
 
+                }
+                else B.addFirst(head);
             }
-            for(int j=N-1;j>=0;j--)
-            {
-                if(B.size()!=0) CA[i][j]=B.pollLast();
+            for (int j = N - 1; j >= 0; j--) {
+                if (B.size() != 0) CA[i][j] = B.pollLast();
             }
         }
         for(int i=0;i<N;i++)
         {
-            for(int j=0;j<N;j++) System.out.print(CA[i][j]);
-            System.out.println();
+            for(int j=0;j<N;j++) A[i][j]=CA[i][j];
         }
+
     }
 
-    static void rotate(String s)
+    static void rotate(int Dir)
     {
-        int Dir=0;
-        if(s.equals("up")) Dir=1;
-        else if(s.equals("down")) Dir=3;
-        else if(s.equals("left")) Dir=2;
-        else if(s.equals("right")) Dir=0;
-
+        int[][] CA ;
         for(int dir=0;dir<Dir;dir++) {
-            int[][] CA = new int[N][N];
-
+            CA = new int[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     CA[i][j] = A[N - j - 1][i];
@@ -78,29 +65,39 @@ public class solution_2048 {
             }
         }
     }
+
+    static void simulation(String s)
+    {
+        int Dir=0;
+        if(s.equals("up")) Dir=1;
+        else if(s.equals("down")) Dir=3;
+        else if(s.equals("left")) Dir=2;
+        else if(s.equals("right")) Dir=0;
+        rotate(Dir);
+        move();
+        rotate((4-Dir)%4);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         T = Integer.parseInt(br.readLine());
-        st= new StringTokenizer(br.readLine());
-        N= Integer.parseInt(st.nextToken());
-        dir = st.nextToken();
-        A= new int[N][N];
-        for(int i=0;i<N;i++)
-        {
+        for(int tc=1;tc<=T;tc++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++) A[i][j]=Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            dir = st.nextToken();
+            A = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) A[i][j] = Integer.parseInt(st.nextToken());
+            }
+            simulation(dir);
+            System.out.println("#"+tc);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) System.out.print(A[i][j] + " ");
+                System.out.println();
+            }
         }
-        System.out.println("rot");
-        move();
-//        rotate(dir);
-//        for(int i=0;i<N;i++)
-//        {
-//            for(int j=0;j<N;j++) System.out.print(A[i][j]+" ");
-//            System.out.println();
-//        }
 
-        System.out.println(T+" "+dir);
     }
 }
